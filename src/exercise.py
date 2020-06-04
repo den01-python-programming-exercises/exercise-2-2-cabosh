0d66e6dabe97e83ad2131396d411dcbb2872200e
cabosh <callum.bosher@gmail.com>
2020-06-04 12:16:51 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
+import numpy as np
+
 def main():
     #write your code below this line
+    number = int(input("Enter a number: "))
+    square_root = np.sqrt(number)
+
+    print(f"The square root of {number} is {square_root}.")
 
 if __name__ == '__main__':
     main()
